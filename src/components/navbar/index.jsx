70b51ac3defb535fe2ac5c0a1828ca4611c3946b
Ashwin <ashwinsrk77@gmail.com>
2023-02-11 19:19:47 +0530

    Initial commit

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,9 @@
+import './navbar.css';
+
+function Navbar() {
+    return (
+        <div>this is navbar</div>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/team/index.jsx
@@ -0,0 +1,12 @@
+import './team.css';
+
+function Team() {
+    return (
+        <div className='team'>
+            <p className='main_team_title'>IIT Roorkee Motorsports Team</p>
+            <p className='main_team_desc'>The people who are making this possible</p>
+        </div>
+    )
+}
+
+export default Team;
